28f7e1dcd4b0076d65eacdb007422fbc745e47fa
Amy Tomey <amy.tomey59@gmail.com>
2021-06-29 13:30:40 -0400

    added form and styling, and js files

---
+++ js/addwords_test.js
@@ -0,0 +1,8 @@
+// form to collect/validate input for the test...
+
+function addWords() {
+    var name = document.forms["chooseWords"]["Name"];
+    var noun = document.forms["chooseWords"]["Noun"];
+    var verb = document.forms["chooseWords"]["TVerb"];
+    var color = document.forms["chooseWords"]["color"];
+}
